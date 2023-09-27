7b3b0d0f5a49650b572cbff069eae56c1225a5b2
vivanne <58852113+vivanne@users.noreply.github.com>
2023-09-27 15:28:42 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,27 @@
 // JavaScript Document
 console.log("hi");
+
+let menubutton = document.querySelector("button");
+let nav = document.querySelector("nav");
+let closing = document.querySelector("#closebutton")
+let vid = document.querySelector("video")
+
+menubutton.onclick = toonMenu;
+
+function toonMenu() {
+    nav.classList.add("toonMenu");
+}
+
+vid.onclick = pauseVideo;
+
+function pauseVideo() {
+    vid.pause();
+}
+
+
+closing.onclick = sluitMenu;
+
+function sluitMenu() {
+    nav.classList.remove("toonMenu");
+}
+
